26c25cc37183a730df70508096fe246c04bff934
Riduidel <nicolas.delsaux@gmail.com>
2012-10-24 14:04:54 +0200

    fixed some doc

--- gaedo-definition/src/main/java/com/dooapp/gaedo/finders/root/FieldInformerLocator.java
+++ gaedo-definition/src/main/java/com/dooapp/gaedo/finders/root/FieldInformerLocator.java
@@ -6,14 +6,14 @@ import com.dooapp.gaedo.finders.FieldInformer;
 import com.dooapp.gaedo.properties.Property;
 
 /**
- * Field informer locator is able to combine all existing informations about a field in order to build the most comprehsive {@link FieldInformer}
+ * Field informer locator is able to combine all existing informations about a field in order to build the most comprehensive {@link FieldInformer}
  * @author Nicolas
  *
  */
 public interface FieldInformerLocator {
 
 	/**
-	 * Given an input {@link Field}, a {@link FieldInformer} will use its own strategy to provide the most meaningfull FieldInformer
+	 * Given an input {@link Property}, a {@link FieldInformer} will use its own strategy to provide the most meaningfull FieldInformer
 	 * @param field
 	 * @return an informer for the field, or null if none can be found
 	 */
