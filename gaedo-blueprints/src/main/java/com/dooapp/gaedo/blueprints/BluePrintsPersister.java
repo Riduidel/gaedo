97802ce33489fa8d4069c4a3b1362926f1eafa1d
Kric ket <cid0680@hotmail.com>
2013-10-01 16:06:27 +0200

    Fixed issue wherein reordering a collection did not work

--- gaedo-blueprints/src/main/java/com/dooapp/gaedo/blueprints/BluePrintsPersister.java
+++ gaedo-blueprints/src/main/java/com/dooapp/gaedo/blueprints/BluePrintsPersister.java
@@ -31,13 +31,8 @@ import com.tinkerpop.blueprints.pgm.Vertex;
 
 public class BluePrintsPersister {
     private static final Logger logger = Logger.getLogger(BluePrintsPersister.class.getName());
-    /**
-     * Node kind used by this updater
-     */
-    private Kind nodeKind;
 
     public BluePrintsPersister(Kind node) {
-        nodeKind = node;
     }
 
 
@@ -331,30 +326,37 @@ public class BluePrintsPersister {
         // As a convention, null values are never stored
         if (value != null /* && value.size()>0 that case precisely created https://github.com/Riduidel/gaedo/issues/13 */) {
             // Get previously existing vertices
-            Iterable<Edge> existingIterator = service.getStrategy().getOutEdgesFor(rootVertex, p);
-            // Do not change previously existing vertices if they correspond to new ones
-            // Which is done in that call : as vertex is always looked up before creation, there is little duplication risk
-            // or at elast that risk should be covered by selected Blueprints implementation
-            Collection<Vertex> newVertices = createCollectionVerticesFor(service, value, cascade, objectsBeingAccessed);
-            Map<Vertex, Edge> oldVertices = new HashMap<Vertex, Edge>();
-            for (Edge e : existingIterator) {
+            Iterable<Edge> existingEdges = service.getStrategy().getOutEdgesFor(rootVertex, p);
+            // Get the new, updated Collection of vertices
+            Collection<Vertex> allVertices = createCollectionVerticesFor(service, value, cascade, objectsBeingAccessed);
+            // Keep track of the edges that correspond to the vertices
+            Map<Vertex, Edge> allEdges = new HashMap<Vertex, Edge>();
+            // ...and put the old, invalid vertices aside for later 
+            Set<Edge> edgesToRemove = new HashSet<Edge>();
+            
+            for (Edge e : existingEdges) {
                 Vertex inVertex = e.getInVertex();
-                if (newVertices.contains(inVertex)) {
-                    newVertices.remove(inVertex);
+                if (allVertices.contains(inVertex)) {
+                    allEdges.put(inVertex, e);
                 } else {
-                    oldVertices.put(inVertex, e);
+                    edgesToRemove.add(e);
+                    allEdges.put(inVertex, null);
                 }
             }
-            // Now the have been collected, remove all old vertices
-            for (Map.Entry<Vertex, Edge> entry : oldVertices.entrySet()) {
-                database.removeEdge(entry.getValue());
+            
+            // First, delete the old ones
+            for (Edge edge : edgesToRemove) {
+                database.removeEdge(edge);
             }
-            // And finally add new vertices
+            
+            // Then, go through the updated Vertices. Create edges if necessary, then always set the order property.
             int order = 0;
-            for (Vertex newVertex : newVertices) {
-                Edge createdEdge = service.getDriver().createEdgeFor(rootVertex, newVertex, p);
+            for (Vertex vertex : allVertices) {
+            	Edge edgeForVertex = allEdges.get(vertex);
+            	if(edgeForVertex == null)
+            		edgeForVertex = service.getDriver().createEdgeFor(rootVertex, vertex, p);
                 // Add a fancy-schmancy property to maintain order in this town
-                createdEdge.setProperty(Properties.collection_index.name(), order++);
+                edgeForVertex.setProperty(Properties.collection_index.name(), order++);
             }
         }
     }
