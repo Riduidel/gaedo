26cba7539abc5658bbe60f60f82838b90d0925db
Riduidel <nicolas.delsaux@gmail.com>
2013-10-22 11:11:33 +0200

    very small optimization to avoid constant getName() calls

--- gaedo-blueprints/src/main/java/com/dooapp/gaedo/blueprints/transformers/AbstractSimpleLiteralTransformer.java
+++ gaedo-blueprints/src/main/java/com/dooapp/gaedo/blueprints/transformers/AbstractSimpleLiteralTransformer.java
@@ -2,10 +2,12 @@ package com.dooapp.gaedo.blueprints.transformers;
 
 public abstract class AbstractSimpleLiteralTransformer<Type> extends AbstractLiteralTransformer<Type>{
 	private final Class type;
+	private final String typeName;
 
 	public AbstractSimpleLiteralTransformer(Class<?> type) {
 		super();
 		this.type = type;
+		this.typeName = type.getName();
 	}
 
 
@@ -16,10 +18,10 @@ public abstract class AbstractSimpleLiteralTransformer<Type> extends AbstractLit
 
 	@Override
 	protected String resolveType(String effectiveType) {
-		return type.getName();
+		return typeName;
 	}
 
 	public boolean canHandle(ClassLoader classLoader, String effectiveType) {
-		return type.getName().equals(effectiveType);
+		return typeName.equals(effectiveType);
 	}
 }
