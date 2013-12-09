8667d8ef86635213500999cd1af246a7b7492c3d
Riduidel <nicolas.delsaux@gmail.com>
2013-12-09 09:33:48 +0100

    this project wass never used, and is not the best way to use gaedo through rest. However, it will stay available in previous versions ....

--- gaedo-rest-server/src/main/java/com/dooapp/gaedo/rest/server/UnrepresentableObjectException.java
+++
@@ -1,9 +0,0 @@
-package com.dooapp.gaedo.rest.server;
-
-class UnrepresentableObjectException extends RestServerException {
-
-	public UnrepresentableObjectException(Object source) {
-		super("No representation can be made out of "+source+" of class "+source.getClass().getName());
-	}
-	
-}
