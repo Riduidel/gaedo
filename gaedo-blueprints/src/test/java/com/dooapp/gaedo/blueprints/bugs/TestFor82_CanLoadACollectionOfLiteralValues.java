58b84311f316be70e699ecc7e90a184c571a7d50
Riduidel <nicolas.delsaux@gmail.com>
2014-03-28 14:04:34 +0100

    Fuck off ! It appears #82 is not fixed regarding collections having no declared size. To fix that, I'll have to change a few things regarding literal value loading

--- gaedo-blueprints/src/test/java/com/dooapp/gaedo/blueprints/bugs/TestFor82_CanLoadACollectionOfLiteralValues.java
+++ gaedo-blueprints/src/test/java/com/dooapp/gaedo/blueprints/bugs/TestFor82_CanLoadACollectionOfLiteralValues.java
@@ -4,16 +4,21 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.persistence.CascadeType;
+
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+import com.dooapp.gaedo.blueprints.AbstractBluePrintsBackedFinderService;
 import com.dooapp.gaedo.blueprints.AbstractGraphEnvironment;
 import com.dooapp.gaedo.blueprints.AbstractGraphPostTest;
+import com.dooapp.gaedo.blueprints.ObjectCache;
 import com.dooapp.gaedo.blueprints.finders.FindPostById;
 import com.dooapp.gaedo.test.beans.Post;
+import com.tinkerpop.blueprints.Vertex;
 
 import static com.dooapp.gaedo.blueprints.TestUtils.simpleTest;
 import static org.hamcrest.Matchers.hasItem;
@@ -61,6 +66,36 @@ public class TestFor82_CanLoadACollectionOfLiteralValues extends AbstractGraphPo
 		tested.extension.add(METHOD_NAME);
 		tested.extension.add(author);
 		tested = (TestFor82_ObjectCollection) getPostService().create(tested);
+		// Now load it .. and hope the collection is correct
+		tested = (TestFor82_ObjectCollection) getPostService().find().matching(new FindPostById(tested.id)).getFirst();
+		assertThat(tested, instanceOf(TestFor82_ObjectCollection.class));
+		assertThat(tested.extension, Matchers.hasItems(METHOD_NAME, author));
+	}
+
+	/**
+	 * Specific test ensuring no NPE can be thrown
+	 */
+	@Test
+	public void a_collection_with_one_string_and_one_object_but_no_size_property_is_ok() {
+		String METHOD_NAME = "a_collection_with_one_string_is_ok";
+		TestFor82_ObjectCollection tested = new TestFor82_ObjectCollection();
+		tested.text = METHOD_NAME;
+		tested.extension.add(METHOD_NAME);
+		tested.extension.add(author);
+		tested = (TestFor82_ObjectCollection) getPostService().create(tested);
+		// before loading it, remove size property to emulate a "legacy"' value
+		AbstractBluePrintsBackedFinderService<?, ?, ?> service = (AbstractBluePrintsBackedFinderService<?, ?, ?>) getPostService();
+		Vertex value = service.getVertexFor(tested, CascadeType.REFRESH, new ObjectCache());
+		// Remvoe all properties ending with size
+		Collection<String> propertiesToRemove = new ArrayList<String>();
+		for(String p : value.getPropertyKeys()) {
+			if(p.endsWith(".size")) {
+				propertiesToRemove.add(p);
+			}
+		}
+		for(String s : propertiesToRemove) {
+			value.removeProperty(s);
+		}
 		// Now load it .. and hope the collection is correct
 		tested = (TestFor82_ObjectCollection) getPostService().find().matching(new FindPostById(tested.id)).getFirst();
 		assertThat(tested, instanceOf(TestFor82_ObjectCollection.class));
