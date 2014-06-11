738f9b1ae64bed784e16e7a474f2ed2a7a4796b2
Riduidel <nicolas.delsaux@gmail.com>
2014-06-11 15:15:25 +0200

    Was missing for #90 associated test

--- gaedo-blueprints/src/test/java/com/dooapp/gaedo/blueprints/beans/PostSubClassInformer.java
+++ gaedo-blueprints/src/test/java/com/dooapp/gaedo/blueprints/beans/PostSubClassInformer.java
@@ -1,7 +1,11 @@
 package com.dooapp.gaedo.blueprints.beans;
 
 import com.dooapp.gaedo.finders.Informer;
+import com.dooapp.gaedo.test.beans.InternalGaedoPostInformer;
+import com.dooapp.gaedo.test.beans.UserInformer;
 
-public interface PostSubClassInformer extends Informer<PostSubClass> {
-	
+public interface PostSubClassInformer extends Informer<PostSubClass>, InternalGaedoPostInformer {
+
+	UserInformer getCreator();
+
 }
